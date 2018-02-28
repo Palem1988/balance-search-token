cd87a38aba4413cfb44036e5e999893d06f1cad1
Pedro Gomes <pedrogomes94@gmail.com>
2018-02-28 21:39:36 +0000

    sort by shorter

--- src/index.js
+++ src/index.js
@@ -149,18 +149,31 @@ class Root extends Component {
             crypto.name.toLowerCase() === _input.toLowerCase() ||
             crypto.symbol.toLowerCase() === _input.toLowerCase()
         );
+        const sortedExactKeys = Object.keys(exactMatch).sort(
+          (a, b) => exactMatch[a].name.length - exactMatch[b].name.length
+        );
+        const sortedExactMatch = sortedExactKeys.map(key => exactMatch[key]);
         const startsWithRegex = new RegExp(`^${_input}`, 'gi');
         const startsWithMatch = cryptoData.filter(
           crypto =>
             crypto.name.toLowerCase().match(startsWithRegex) ||
             crypto.symbol.toLowerCase().match(startsWithRegex)
         );
+        const sortedStartsWithKeys = Object.keys(startsWithMatch).sort(
+          (a, b) => startsWithMatch[a].name.length - startsWithMatch[b].name.length
+        );
+        console.log(startsWithMatch);
+        const sortedStartsWithMatch = sortedStartsWithKeys.map(key => startsWithMatch[key]);
         const anyMatch = cryptoData.filter(
           crypto =>
             crypto.name.toLowerCase().match(_input.toLowerCase()) ||
             crypto.symbol.toLowerCase().match(_input.toLowerCase())
         );
-        const list = _.unionBy(exactMatch, startsWithMatch, anyMatch, 'id');
+        const sortedAnyKeys = Object.keys(anyMatch).sort(
+          (a, b) => anyMatch[a].name.length - anyMatch[b].name.length
+        );
+        const sortedAnyMatch = sortedAnyKeys.map(key => anyMatch[key]);
+        const list = _.unionBy(sortedExactMatch, sortedStartsWithMatch, sortedAnyMatch, 'id');
         this.setState({ list, loading: false });
       }, this.state.delay);
     } else {
